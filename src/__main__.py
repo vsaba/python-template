5bc9084b02ba84456baca7215c2f0902f22aac25
Vito Sabalic <vito.sabalic@gmail.com>
2024-11-04 21:37:39 +0100

    feat: add env configuration loading

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,24 @@
+"""
+This is the main module used for running the project as a whole
+"""
+
 from src.config import config
 
 
 def main():
     print(f"Application version: {config.App.VERSION}")
     print(f"Application environment: {config.App.DEVELOPMENT}")
+    test("Hello world")
+
+
+def test(a):
+    print(a)
 
 
 if __name__ == "__main__":
     # parse arguments
 
     main()
+
+
+# TODO must enable linting, formatting, type checking and docstring generating
--- src/config/config.py
+++ src/config/config.py
@@ -7,3 +7,7 @@ load_dotenv()
 class App:
     VERSION = os.environ["APP_VERSION"]
     DEVELOPMENT = os.environ["APP_ENVIRONMENT"]
+
+
+def test(a: str) -> None:
+    pass
